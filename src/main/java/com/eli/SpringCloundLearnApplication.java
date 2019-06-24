66e93091e86e271550d6499fa4b7b5d4f29fa891
pekah <474705863@qq.com>
2019-06-24 22:53:32 +0800

    log

--- src/main/java/com/eli/SpringCloundLearnApplication.java
+++ src/main/java/com/eli/SpringCloundLearnApplication.java
@@ -1,13 +1,28 @@
 package com.eli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringCloundLearnApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringCloundLearnApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloundLearnApplication.class, args);
+	}
+
+	@PostConstruct
+	public void log(){
+		logger.trace("trace log");
+		logger.debug("debug log");
+		logger.info("info log");
+		logger.warn("warn log");
+		logger.error("error log");
 	}
 
 }
